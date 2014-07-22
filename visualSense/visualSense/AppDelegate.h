29f9e35ceea0af21484989490d8c3b55292b9cd0
管伟东 <631935859@qq.com>
2014-07-22 14:55:35 +0800

    第一次创建项目

---
+++ visualSense/visualSense/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  visualSense
+//
+//  Created by 管伟东 on 14-7-22.
+//  Copyright (c) 2014年 guanweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ visualSense/visualSense/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  visualSense
+//
+//  Created by 管伟东 on 14-7-22.
+//  Copyright (c) 2014年 guanweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
